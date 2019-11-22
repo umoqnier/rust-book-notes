81558d45b4a3b43e413ed42a1bfc44db0a0d78b9
Diego Alberto Barriga Martínez <diegobarriga@protonmail.com>
2019-11-22 09:40:26 -0600

    update(adivina): Números aleatorios y comparación
    	Se agrega la generación de número aleatorios y la comparación
    	del número con la entrada de la usuaria

--- adivida_adivinador/src/main.rs
+++ adivida_adivinador/src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
-
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    println!("¡Adivina el número!");
+    println!("¡Adivina el número entre 1 y 100!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("Ingresa tu estimación: ");
 
@@ -10,6 +13,17 @@ fn main() {
 
     io::stdin().read_line(&mut guess)
         .expect("ERROR al leer la línea");
-    
+   
+    let guess: u32 = guess.trim().parse()
+        .expect("Solo se aceptan numeros. ¿Me quieres ver la cara de estúpida?");
+
     println!("Tu estimación>> {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Muy bajo"),
+        Ordering::Greater => println!("Muy alto"),
+        Ordering::Equal => println!("Ganaste!!! :-)"),
+    }
 }
+
+
